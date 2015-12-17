ea03f9ef614cbb00540ee34a9007448f5e2bf2d4
dougtebay <dougtebay@gmail.com>
2015-12-17 11:01:22 -0500

    All tests passing

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
 # code your solution here
+def power_digit_sum(base, exponent)
+  total = 1
+  sum = 0
+  exponent.times { total *= base }
+  total = total.to_s.split("")
+  total.each { |i| sum += i.to_i }
+  sum
+end
